690c336b85e9fa04c72b137de563c697b5882eed
isnot <naoto@isnot.jp>
2023-09-05 15:54:14 +0900

    docs: add docstring

--- mermaid2django/parser.py
+++ mermaid2django/parser.py
@@ -3,6 +3,23 @@ from mermaid2django.relationship import RelationshipItem, RelationshipSet
 
 
 class ParseMermaidErDiagram:
+    """
+    Mermaid記法で記述したER図を、字句解析および構文解析します
+
+    Mermaid.jsのサブセットです。独自の拡張と制限、未対応があります
+
+    Params
+    -------
+    <str>
+        ER図を記述したファイルのパス・ファイル名
+
+    Raises
+    ------
+    TypeError
+        内部エラーです。LEX_TOKEN定義にエラーがある可能性
+    RuntimeError
+        未対応の形式です。入力を解析できませんでした。
+    """
     LEX_TOKENS = (
         {
             "type": "blank_line",
@@ -42,6 +59,15 @@ class ParseMermaidErDiagram:
     )
 
     def __init__(self, *args, **kwargs):
+        """
+        __init__ _summary_
+
+        インスタンス変数:
+            entities: <dict>
+            relations: <list<>>
+            relationship_set: <RelationshipSet>
+            input_filename: <str>
+        """
         self.entities = {}
         self.relations = []
         self.relationship_set = None  # RelationshipSet
@@ -54,6 +80,17 @@ class ParseMermaidErDiagram:
         self.__now_entity_name = ""
 
     def read_input(self):
+        """
+        read_input ファイルの内容を読み込む
+
+        全体をいっぺんに読み込みます。
+        STREAMINGではありません。、
+
+        Returns
+        -------
+        <list<str>>
+            ファイルの中身（行指向）
+        """
         try:
             fd = open(self.input_filename, "r", encoding="utf-8")
             lines = fd.readlines()
@@ -80,6 +117,28 @@ class ParseMermaidErDiagram:
             return self.entities[self.__now_entity_name]
 
     def split_tokenizer(self, source="", options={}):
+        """
+        split_tokenizer 1行をトークンに分割します
+
+        parse()の中から呼ばれます。このメソッド単体で使うことはありません
+
+        Parameters
+        ----------
+        source : str, optional
+            1行分の文字列, by default ""
+        options : dict, optional
+            定義の要素, by default {}
+
+        Returns
+        -------
+        <list<str>>
+            文字列を分割したもの
+
+        Raises
+        ------
+        TypeError
+            定義の違反
+        """
         if "split" not in options:
             return [source]
         if "sep" not in options["split"]:
@@ -94,6 +153,22 @@ class ParseMermaidErDiagram:
             return source.split(options["split"]["sep"])
 
     def lexicalize_single(self, line=""):
+        """
+        lexicalize_single 1行の字句解析
+
+        クラス変数LEX_TOKENSで定義した内容
+        parse()の中から呼ばれます。このメソッド単体で使うことはありません
+
+        Parameters
+        ----------
+        line : str, optional
+            1行分の入力文字列, by default ""
+
+        Returns
+        -------
+        tuple<<str>, <list<str>>>
+            字句解析の結果
+        """
         line = line.strip().rstrip("\n")
         for item in ParseMermaidErDiagram.LEX_TOKENS:
             parser_command = f"on_{item['type']}"
@@ -114,6 +189,16 @@ class ParseMermaidErDiagram:
         )
 
     def parse(self):
+        """
+        parse 実際にファイルから内容を読み込んで解析します
+
+        解析結果をインスタンスに保持します
+
+        Returns
+        -------
+        <list<Entity>>
+            エンティティのリスト
+        """
         self.on_start()
         for line in self.read_input():
             parser_command, lexicalized = self.lexicalize_single(line)
@@ -124,21 +209,61 @@ class ParseMermaidErDiagram:
         return self.get_entities()
 
     def on_start(self):
+        """
+        on_start 解析処理全体の始まり
+
+        ファイルの先頭
+        """
         pass
 
     def on_pre_command(self):
+        """
+        on_pre_command 行毎の処理（開始前）
+
+        1行の先頭
+        """
         pass
 
     def on_post_command(self):
+        """
+        on_post_command 行毎の処理（完了後）
+
+        一行の末尾
+        """
         pass
 
     def on_finish(self):
+        """
+        on_finish 解析処理全体の終わり
+
+        ファイルの末尾
+        """
         self.relationship_set = RelationshipSet(self.relations)
 
     def on_comment(self, line=[]):
+        """
+        on_comment コメント行
+
+        %% で始まる行
+
+        Parameters
+        ----------
+        line : list, optional
+            字句解析済みデータ, by default []
+        """
         self.__last_comment += line[0].lstrip("%").strip()
 
     def on_entity_start(self, line=[]):
+        """
+        on_entity_start エンティティ定義ブロックの開始
+
+        ブロックは「{}」で囲まれた部分
+
+        Parameters
+        ----------
+        line : list, optional
+            字句解析済みデータ, by default []
+        """
         name = line[0].rstrip(" {")
         self.__now_entity_name = name
         if name not in self.entities.keys():
@@ -149,9 +274,25 @@ class ParseMermaidErDiagram:
         self.__last_comment = ""
 
     def on_entity_end(self, _):
+        """
+        on_entity_end エンティティ定義ブロックの終わり
+
+        Parameters
+        ----------
+        _ : Any
+            単に無視します
+        """
         self.__now_entity_name = ""
 
     def on_entity_item(self, line=[]):
+        """
+        on_entity_item 属性（アトリビュート）の1行分の処理
+
+        Parameters
+        ----------
+        line : list, optional
+            字句解析済みデータ, by default []
+        """
         m = self.get_current_entity()
         verbose = line[2].strip('"')
         if verbose.find("PK") != -1:
@@ -177,14 +318,45 @@ class ParseMermaidErDiagram:
         self.__last_comment = ""
 
     def on_relationship(self, items=[]):
+        """
+        on_relationship リレーションシップ、1行分の処理
+
+        エンティティ・ブロックの内部に記述することはできない
+
+        Parameters
+        ----------
+        items : list, optional
+            字句解析済みデータ, by default []
+        """
         cad = RelationshipItem(items, annotation=self.__last_comment)
         cad.parse()
         self.relations.append(cad)
         self.__last_comment = ""
 
     def on_blank_line(self, _):
+        """
+        on_blank_line 空行の処理
+
+        Parameters
+        ----------
+        _ : Any
+            単に無視します
+        """
         self.__last_comment = ""
 
     def on_unknown(self, _):
+        """
+        on_unknown 未知の形式
+
+        Parameters
+        ----------
+        _ : Any
+            単に無視します
+
+        Raises
+        ------
+        RuntimeError
+            字句解析の時点で未対応の形式だった場合
+        """
         if _[0] != "erDiagram":
             raise RuntimeError("unknown token", _)
--- mermaid2django/relationship.py
+++ mermaid2django/relationship.py
@@ -1,4 +1,13 @@
 class RelationshipExpression:
+    """
+    リレーションを表す文字列を分析する
+
+    入力の例：※6文字、空白文字を含まない
+        ||--||
+        |o..o|
+        |o--|{
+        }|--|{
+    """
     def __init__(self, piece=""):
         if len(piece) != 6:
             raise TypeError("missmatch")
@@ -44,6 +53,13 @@ class RelationshipExpression:
 
 
 class RelationshipItem:
+    """
+    リレーションシップの定義1件（1行）＋アノテーション
+
+    「:」の前後には必ず空白文字を入れる
+    説明文はダブルクォーテーションで囲む。省略する場合は、「""」
+    アノテーションの冒頭1ワード（空白文字で区切る）は、特別に属性名とみなす
+    """
     def __init__(self, items=[], annotation=""):
         (
             left,
@@ -91,6 +107,25 @@ class RelationshipItem:
 
 
 class RelationshipSet:
+    """
+    ER図の中にある、複数のリレーションシップ全体
+
+    制限事項
+    エンティティ名に「__」を含んで、__の両辺がエンティティ名であるとき、
+    所謂「リンクテーブル」とみなします。
+
+    （未実装）NULLABLEを見ていない
+
+    Params
+    -------
+    <list<RelationshipItem>>
+        ひとつの図に含まれる全てのリレーションシップ
+
+    Raises
+    ------
+    RuntimeError
+        インスタンス生成時点での属性名にない文字列で検索しようとした
+    """
     ALL_ENTITY_NAMES = set()
 
     def __init__(self, all=[]):
@@ -112,6 +147,16 @@ class RelationshipSet:
         return e.type == "one2one"
 
     def get_relations(self):
+        """
+        全てのリレーションシップを取得する
+
+        setであり重複はない
+
+        Returns
+        -------
+        <set<RelationshipItem>>
+            インスタンス生成時に受け入れたリレーションシップ全て
+        """
         return self.relations
 
     def is_link_table(self, entity_name=""):
